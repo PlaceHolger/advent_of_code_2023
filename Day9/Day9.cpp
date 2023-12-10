a6972b8a8b3a6d2270e10c544eb85e0da6b738ae
Holger Durach <holger.durach@unity3d.com>
2023-12-10 01:19:23 +0100

    fixed day9... this took me way to long : ) + part2

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <iostream>
 
 #include "DataDay9.h"
@@ -7,27 +6,63 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    long long sumPredictions = 0;
+    constexpr bool IS_PART1 = true;
     
-    for (auto& currentRow : s_Data)
-    {
-        unsigned int numElements = NUM_NUMBERS; //we need one element less with each level of prediction
+    int64_t sumPredictions = 0;
 
-        int currentPredictionSum = 666;
-        while (currentPredictionSum != 0)
+    if(IS_PART1)
+    {
+        for (auto& currentRow : s_Data)
         {
-            currentPredictionSum = 0;
-            for (unsigned int j = 0; j < numElements - 1; ++j)
+            unsigned int numElements = NUM_NUMBERS; //we need one element less with each level of prediction
+
+            bool allZero = false; //if all elements are zero, we can stop
+            while (!allZero)
             {
-                const int difference = currentRow[j+1] - currentRow[j];
-                currentRow[j] = difference; //we don't need the original numbers anymore
-                currentPredictionSum += difference;
+                allZero = true;
+                for (unsigned int j = 0; j < numElements - 1; ++j)
+                {
+                    const int difference = currentRow[j+1] - currentRow[j];
+                    currentRow[j] = difference; //we don't need the original numbers anymore
+                    allZero = (difference == 0) && allZero;
+                    // cout << difference << " ";
+                }
+                numElements--;
+               // cout << endl;
             }
-            numElements--;
-        }
-        //now we have the predictions for the current line, our data already only contains the last element of each prediction-level, so we just need to add them up
-        for (const int i : currentRow)
-            sumPredictions += i;
+            for (const int i : currentRow)
+                sumPredictions += i;
+        } 
+    }
+    else
+    {
+        for (auto& currentRow : s_Data)
+        {
+            int startElement = 1; //we need one element less with each level of prediction
+
+            bool allZero = false; //if all elements are zero, we can stop
+            while (!allZero)
+            {
+                allZero = true;
+                for (int j = NUM_NUMBERS - 1; j >= startElement; --j)
+                {
+                    const int difference = currentRow[j] - currentRow[j - 1];
+                    currentRow[j] = difference; //we don't need the original numbers anymore
+                    allZero = (difference == 0) && allZero;
+                    //cout << difference << " ";
+                }
+                startElement++;
+                //cout << endl;
+            }
+
+            //iterate from the back to the front and subtract the previous element
+            int lastValue = 0;
+            for (int i = NUM_NUMBERS - 1; i >= 0; --i)
+            {
+                lastValue = currentRow[i] - lastValue;
+            }
+            sumPredictions += lastValue;
+        } 
     }
     
     std::cout << "Sum of all predictions: " << sumPredictions << std::endl;
