425cac3d9f4b35716f343c007f9bef9afc5f7c32
Holger Durach <holger.durach@unity3d.com>
2024-01-05 17:59:48 +0100

    fixed Day17 :)

--- Day17/Day17.cpp
+++ Day17/Day17.cpp
@@ -8,10 +8,10 @@ constexpr int NUM_ROWS = std::size(s_Data);
 
 enum class Direction : char
 {
-    Left,
-    Right,
-    Up,
-    Down,
+    Left = 0,
+    Right = 1,
+    Up = 2,
+    Down = 3,
     NONE,
 };
 
@@ -106,8 +106,10 @@ void PrintMapWithPath(const Node* const p_TargetNode = nullptr)
             {
                 std::cout << "\033[1;31m " << GetCharForDirection(p_PathNode->lastDirections[2]);
             }
-            else 
-                std::cout << "\033[0m " << s_Data[y][x];
+            else
+            {
+                std::cout << "\033[0m " << s_Data[y][x];                
+            }
         }
         std::cout << "\n";
     }
@@ -128,16 +130,18 @@ void PrintMap(int highlightX = -1, int highlightY = -1)
                 for(int j = 0; j < NUM_DIRECTIONS; ++j)
                 {
                     isInOpen |= std::find(openList.begin(), openList.end(), &nodes[i][j][y * NUM_COLUMNS + x]) != openList.end();
-                    isInClosed |= std::find(closedList.begin(), closedList.end(), &nodes[i][j][y * NUM_COLUMNS + x]) != closedList.end();
+                    isInClosed |= std::find(closedList.begin(), closedList.end(), &nodes[i][j][y * NUM_COLUMNS + x]) != closedList.end();                    
                 }
             }
 
             if(x == highlightX && y == highlightY)
                 std::cout << "\033[1;33m " << s_Data[y][x];
-            else if (isInOpen)
-                std::cout << "\033[1;32m " << s_Data[y][x];
+            else if(isInClosed && isInOpen)
+                std::cout << "\033[1;34m " << s_Data[y][x];
             else if (isInClosed)
                 std::cout << "\033[1;31m " << s_Data[y][x];
+            else if (isInOpen)
+                std::cout << "\033[1;32m " << s_Data[y][x];
             else 
                 std::cout << "\033[0m " << s_Data[y][x];
         }
@@ -175,7 +179,8 @@ void CalculatePath(int startX, int startY, int targetX, int targetY)
     {
         Node* p_CurrentNode = GetLowestCostNode();
 
-        //PrintMap(p_CurrentNode->posX, p_CurrentNode->posY);
+        //if(openList.size() % 500 == 0)
+          //  PrintMap(p_CurrentNode->posX, p_CurrentNode->posY);
 
         if (p_CurrentNode->posX == targetX && p_CurrentNode->posY == targetY)
         {
@@ -187,21 +192,23 @@ void CalculatePath(int startX, int startY, int targetX, int targetY)
         closedList.push_back(p_CurrentNode);
         //we count how many times we went in the same direction, we do so by starting at the end of the lastDirections array and going back until we find a different direction
         const int numSameDirection = CalcNumSameDirections(p_CurrentNode);
-        const int lastDir = static_cast<int>(p_CurrentNode->lastDirections[2]);
+        const Direction lastDirEnum = p_CurrentNode->lastDirections[2];
+        const int lastDir = static_cast<int>(lastDirEnum);
 
+        int numDirPlus1 = std::min(MAX_SAME_DIRECTION - 1, numSameDirection+1); //clamped, will be removed anyway in the next step.
         //get the current node's neighbours
         Node* p_Neighbours[4] = { nullptr, nullptr, nullptr, nullptr };
         if (p_CurrentNode->posX > 0) //left
-            p_Neighbours[0] = &nodes[numSameDirection][lastDir][(p_CurrentNode->posY) * NUM_COLUMNS + (p_CurrentNode->posX - 1)];
+            p_Neighbours[0] = &nodes[(lastDirEnum == Direction::Left) ? numDirPlus1 : 0][lastDir][p_CurrentNode->posY * NUM_COLUMNS + (p_CurrentNode->posX - 1)];
         if (p_CurrentNode->posX < NUM_COLUMNS - 1) //right
-            p_Neighbours[1] = &nodes[numSameDirection][lastDir][(p_CurrentNode->posY) * NUM_COLUMNS + (p_CurrentNode->posX + 1)];
+            p_Neighbours[1] = &nodes[(lastDirEnum == Direction::Right) ? numDirPlus1 : 0][lastDir][p_CurrentNode->posY * NUM_COLUMNS + (p_CurrentNode->posX + 1)];
         if (p_CurrentNode->posY > 0) //up
-            p_Neighbours[2] = &nodes[numSameDirection][lastDir][(p_CurrentNode->posY - 1) * NUM_COLUMNS + (p_CurrentNode->posX)];
+            p_Neighbours[2] = &nodes[(lastDirEnum == Direction::Up) ? numDirPlus1 : 0][lastDir][(p_CurrentNode->posY - 1) * NUM_COLUMNS + p_CurrentNode->posX];
         if (p_CurrentNode->posY < NUM_ROWS - 1) //down
-            p_Neighbours[3] = &nodes[numSameDirection][lastDir][(p_CurrentNode->posY + 1) * NUM_COLUMNS + (p_CurrentNode->posX)];
+            p_Neighbours[3] = &nodes[(lastDirEnum == Direction::Down) ? numDirPlus1 : 0][lastDir][(p_CurrentNode->posY + 1) * NUM_COLUMNS + p_CurrentNode->posX];
 
         //we dont check the neighbour in the same direction we came from
-        switch (p_CurrentNode->lastDirections[2])
+        switch (lastDirEnum)
         {
             case Direction::Left:
                 p_Neighbours[1] = nullptr; //remove right neighbour
@@ -215,35 +222,19 @@ void CalculatePath(int startX, int startY, int targetX, int targetY)
             case Direction::Down:
                 p_Neighbours[2] = nullptr;
                 break;
+            case Direction::NONE:
+                break;
         }
 
-        if(numSameDirection == MAX_SAME_DIRECTION - 1)
-        {
+        if(numSameDirection >= MAX_SAME_DIRECTION - 1)
+        {            
             //we can't go in the same direction again, so we remove the neighbour in the same direction as the last 3 directions
-            switch (p_CurrentNode->lastDirections[2])
-            {
-                case Direction::Left:
-                    p_Neighbours[0] = nullptr; //remove left neighbour
-                    break;
-                case Direction::Right:
-                    p_Neighbours[1] = nullptr;
-                    break;
-                case Direction::Up:
-                    p_Neighbours[2] = nullptr;
-                    break;
-                case Direction::Down:
-                    p_Neighbours[3] = nullptr;
-                    break;
-                default: //should never happen
-                    assert(false);
-            }
+            p_Neighbours[lastDir] = nullptr;
         }
 
         //for each neighbour we calculate the cost to get to it
         for (int i = 0; i < 4; ++i)
-        {
-            const Direction direction = static_cast<Direction>(i); //the direction we are going to take to get to the neighbour
-            
+        {            
             if (p_Neighbours[i] == nullptr)
                 continue;
 
@@ -268,7 +259,7 @@ void CalculatePath(int startX, int startY, int targetX, int targetY)
                 p_Neighbours[i]->prev = p_CurrentNode;
                 p_Neighbours[i]->lastDirections[0] = p_CurrentNode->lastDirections[1];
                 p_Neighbours[i]->lastDirections[1] = p_CurrentNode->lastDirections[2];
-                p_Neighbours[i]->lastDirections[2] = direction;
+                p_Neighbours[i]->lastDirections[2] = static_cast<Direction>(i); //the direction we are going to take to get to the neighbour
 
                 //check if the neighbour is in the open list
                 const auto openListIt = std::find(openList.begin(), openList.end(), p_Neighbours[i]);
@@ -319,6 +310,7 @@ int main(int argc, char* argv[])
     // //now we have the shortest path, we can print the cost for the last node
     std::cout << "Shortest path cost: " << p_BestTargetNode->cost << std::endl;
     //
+    
     PrintMapWithPath(p_BestTargetNode);
     
     return 0;
